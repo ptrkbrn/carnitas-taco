d958687e7880e352b21ac39c99d40133cca472bb
Patrick Breen <pressure78@gmail.com>
2021-06-06 00:36:15 -0400

    lint js. fix fadeIn transition.

--- index.js
+++ index.js
@@ -1,123 +1,121 @@
-const pages = document.getElementsByClassName("page");
+const pages = document.getElementsByClassName('page');
 
 // handles navigation between pages
 function changePage(e) {
-    const selectedPage = e.value;
-    const btnArray = Array.from(document.getElementsByClassName('nav-btn'));
-    btnArray.map(button => button.classList.remove('selected'));
-    e.classList.add('selected')
-    // document.getElementById(selectedPage).classList.add('fadeIn');
-    Array.from(pages).map(page => page.classList.add('hidden'));
-    document.getElementById(selectedPage).classList.remove('hidden');
-    // document.getElementById(selectedPage).classList.add('fadeIn');
+  const selectedPage = e.value;
+  const btnArray = Array.from(document.getElementsByClassName('nav-btn'));
+  btnArray.map((button) => button.classList.remove('selected'));
+  e.classList.add('selected');
+  // document.getElementById(selectedPage).classList.add('fadeIn');
+  Array.from(pages).map((page) => page.classList.add('hidden'));
+  document.getElementById(selectedPage).classList.remove('hidden');
+  // document.getElementById(selectedPage).classList.add('fadeIn');
 
-    let stateData = { selectedPage };
+  const stateData = { selectedPage };
 
-    // adds page navigation to browser history
-    history.pushState(stateData, '', `/${selectedPage}`);;
-};
+  // adds page navigation to browser history
+  history.pushState(stateData, '', `/${selectedPage}`);
+}
 
 // allows navigation back
-window.onpopstate = function(e) {
-    console.log(e)
-    const lastPage = document.body.querySelector(`button[value='${e.state.selectedPage}']`)
-    if (e.state != null) {
-        changePage(lastPage);
-    }
-}
+window.onpopstate = function (e) {
+  console.log(e);
+  const lastPage = document.body.querySelector(`button[value='${e.state.selectedPage}']`);
+  if (e.state != null) {
+    changePage(lastPage);
+  }
+};
 
 // toggles dark mode
-setTheme = (theme) => {
-    let darkMode = theme;
-    if (darkMode) {
-        Array.from(document.querySelectorAll("*")).map(el => el.classList.add('darkMode'));
-        Array.from(document.querySelectorAll("*")).map(el => el.classList.remove('lightMode'));
-        // toggles which button is selected
-        document.getElementById('dark-mode-btn').classList.add('selected');
-        document.getElementById('light-mode-btn').classList.remove('selected');
-        document.querySelector('img').style.borderColor = "white";
+const setTheme = (theme) => {
+  const darkMode = theme;
+  if (darkMode) {
+    Array.from(document.querySelectorAll('*')).map((el) => el.classList.add('darkMode'));
+    Array.from(document.querySelectorAll('*')).map((el) => el.classList.remove('lightMode'));
+    // toggles which button is selected
+    document.getElementById('dark-mode-btn').classList.add('selected');
+    document.getElementById('light-mode-btn').classList.remove('selected');
+    document.querySelector('img').style.borderColor = 'white';
+  } else {
+    Array.from(document.querySelectorAll('*')).map((el) => el.classList.add('lightMode'));
+    Array.from(document.querySelectorAll('*')).map((el) => el.classList.remove('darkMode'));
+    // toggles which button is selected
+    document.getElementById('light-mode-btn').classList.add('selected');
+    document.getElementById('dark-mode-btn').classList.remove('selected');
+    document.querySelector('img').style.borderColor = 'black';
+  }
+};
 
-    } else {
-        Array.from(document.querySelectorAll("*")).map(el => el.classList.add('lightMode'));
-        Array.from(document.querySelectorAll("*")).map(el => el.classList.remove('darkMode'));
-        // toggles which button is selected
-        document.getElementById('light-mode-btn').classList.add('selected');
-        document.getElementById('dark-mode-btn').classList.remove('selected');      
-        document.querySelector('img').style.borderColor = "black";           
-    }
-    return;
+// shortens project text to preview length, trims extraneous whitespace, and appends an ellipses
+function getPreview(text) {
+  return `${text.slice(0, 100).trim()}...`;
 }
 
-
-window.onload = function() {            
-    // shortens full project text to preview length, trims extraneous whitespace, and appends an ellipses
-    function getPreview(text) {
-        return text.slice(0, 100).trim() + "...";
-    }
-
-    // generates project tile element for each object in the projects array
-    projects.map(project => {
-        let preview = getPreview(project.description);
-        let projectTile = document.createElement('div');
-        let header = document.createElement('div');
-        let title = document.createElement('h3');
-        let hr = document.createElement('hr');
-        let description = document.createElement('div');
-        let projectText = document.createElement('p');
-        let button = document.createElement('button');
-        let footer = document.createElement('footer');
-        let demoLink = document.createElement('a');
-        let codeLink = document.createElement('a');
-        title.innerText = project.title;
-        projectTile.setAttribute('class', 'project-tile');
-        header.setAttribute('class', 'project-header');
-        description.setAttribute('class', "project-description");
-        projectText.setAttribute('class', 'project-text');
-        projectText.innerText = preview;
-        button.setAttribute('class', 'toggle-text');
-        button.setAttribute('onclick', 'showMore(this)');
-        button.innerText = "Show more";
-        demoLink.innerText = "See it";
-        codeLink.innerText = "GitHub";
-        demoLink.setAttribute('href', project.liveLink);
-        codeLink.setAttribute('href', project.codeLink);
-
-        projectTile.append(header);
-        header.append(title);
-        projectTile.append(hr);
-        projectTile.append(description);
-        description.append(projectText);
-        projectTile.append(button);
-        projectTile.append(footer);
-        projectTile.append(hr);
-        footer.append(demoLink);
-        footer.append(" | ");
-        footer.append(codeLink);
-
-        // appends each project tile to the content element
-        document.querySelector('.content').append(projectTile);
-    });
-
-    // handles expanding/collapsing project tile description text
-    showMore = (e) => {
-        let currentProject = projects.filter(project => project.title === e.parentElement.children[0].children[0].innerText);
-        currentProject = currentProject[0];
-        let displayedText = e.parentElement.children[1].children[0];
-        // if preview text is displayed, toggle to full text
-        if (displayedText.innerText === getPreview(currentProject.description)) {
-            displayedText.innerText = currentProject.description;
-            e.innerText = "Show less";
-            return;
-        // otherwise, toggle to preview text
-        } else {
-            displayedText.innerText = getPreview(currentProject.description);
-            e.innerText = "Show more";
-            return;
-        }
-    }
-    // sets inital page theme based on browser preferences
-    // setTheme(false);
-    setTheme(window.matchMedia('(prefers-color-scheme: dark)').matches);
-
-    setTimeout(() => {Array.from(document.querySelectorAll('*')).map(el => el.classList.add('fadeIn'))}, 500);
+// handles expanding/collapsing project tile description text
+function showMore(e) {
+  let currentProject = projects.filter(
+    (project) => project.title === e.parentElement.children[0].children[0].innerText,
+  );
+  [currentProject] = currentProject;
+  const displayedText = e.parentElement.children[1].children[0];
+  // if preview text is displayed, toggle to full text
+  if (displayedText.innerText === getPreview(currentProject.description)) {
+    displayedText.innerText = currentProject.description;
+    e.innerText = 'Show less';
+    // otherwise, toggle to preview text
+  } else {
+    displayedText.innerText = getPreview(currentProject.description);
+    e.innerText = 'Show more';
+  }
 }
+
+window.onload = function () {
+  // generates project tile element for each object in the projects array
+  projects.forEach((project) => {
+    const preview = getPreview(project.description);
+    const projectTile = document.createElement('div');
+    const header = document.createElement('div');
+    const title = document.createElement('h3');
+    const hr = document.createElement('hr');
+    const description = document.createElement('div');
+    const projectText = document.createElement('p');
+    const button = document.createElement('button');
+    const footer = document.createElement('footer');
+    const demoLink = document.createElement('a');
+    const codeLink = document.createElement('a');
+    title.innerText = project.title;
+    projectTile.setAttribute('class', 'project-tile');
+    header.setAttribute('class', 'project-header');
+    description.setAttribute('class', 'project-description');
+    projectText.setAttribute('class', 'project-text');
+    projectText.innerText = preview;
+    button.setAttribute('class', 'toggle-text');
+    button.setAttribute('onclick', 'showMore(this)');
+    button.innerText = 'Show more';
+    demoLink.innerText = 'See it';
+    codeLink.innerText = 'GitHub';
+    demoLink.setAttribute('href', project.liveLink);
+    codeLink.setAttribute('href', project.codeLink);
+
+    projectTile.append(header);
+    header.append(title);
+    projectTile.append(hr);
+    projectTile.append(description);
+    description.append(projectText);
+    projectTile.append(button);
+    projectTile.append(footer);
+    projectTile.append(hr);
+    footer.append(demoLink);
+    footer.append(' | ');
+    footer.append(codeLink);
+
+    // appends each project tile to the content element
+    document.querySelector('.content').append(projectTile);
+  });
+
+  // sets inital page theme based on browser preferences
+  // setTheme(false);
+  setTheme(window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+  setTimeout(() => { Array.from(document.querySelectorAll('*')).map((el) => el.classList.add('fadeIn')); }, 500);
+};
--- projects.js
+++ projects.js
@@ -1,20 +1,20 @@
 const projects = [
-    {
-    title: "Frasier Title Card Generator",
-    description: "An app that allows users to generate their own custom images inspired by the interstitial title cards on the hit 90s sitcom \"Frasier.\" A WYSIWYG rich single page application, built with react and utilizing a third-party library to capture the image. Featuring custom SVGs and pure CSS animations. Featured on The Takeout.",
-    liveLink: "https://ptrkbrn.github.io/aggressive-zinfandel/",
-    codeLink: "https://github.com/ptrkbrn/aggressive-zinfandel",
-    },
-    {
-        title: "CRWS Beer App",
-        description: "Built with Python and Flask, with a Postgres database. Designed and built for Country Roads Wine & Spirits. Users can add, update, and remove breweries, beers and distributors, as well as upload information such as price, ABV as well as images. When a user adds a new brewery to the database, the information is first checked against a third-party API to ensure it's accurate and formatted correctly. Image hosting is handled with AWS. Deployed on Heroku. Final project for Harvard CS50.",
-        liveLink: "https://guarded-thicket-25407.herokuapp.com/",
-        codeLink: "https://github.com/ptrkbrn/dollar-slice",
-    },
-    {
-        title: "React Todo App",
-        description: "Todo list application built with React. Mobile-first, responsive design. Users can add, update, and remove todos. App uses LocalStorage for basic data persistence.",
-        liveLink: "ptrkbrn.com",
-        codeLink: "https://github.com/ptrkbrn/todo-list",
-    }
-]
+  {
+    title: 'Frasier Title Card Generator',
+    description: 'An app that allows users to generate their own custom images inspired by the interstitial title cards on the hit 90s sitcom "Frasier." A WYSIWYG rich single page application, built with react and utilizing a third-party library to capture the image. Featuring custom SVGs and pure CSS animations. Featured on The Takeout.',
+    liveLink: 'https://ptrkbrn.github.io/aggressive-zinfandel/',
+    codeLink: 'https://github.com/ptrkbrn/aggressive-zinfandel',
+  },
+  {
+    title: 'CRWS Beer App',
+    description: "Built with Python and Flask, with a Postgres database. Designed and built for Country Roads Wine & Spirits. Users can add, update, and remove breweries, beers and distributors, as well as upload information such as price, ABV as well as images. When a user adds a new brewery to the database, the information is first checked against a third-party API to ensure it's accurate and formatted correctly. Image hosting is handled with AWS. Deployed on Heroku. Final project for Harvard CS50.",
+    liveLink: 'https://guarded-thicket-25407.herokuapp.com/',
+    codeLink: 'https://github.com/ptrkbrn/dollar-slice',
+  },
+  {
+    title: 'React Todo App',
+    description: 'Todo list application built with React. Mobile-first, responsive design. Users can add, update, and remove todos. App uses LocalStorage for basic data persistence.',
+    liveLink: 'ptrkbrn.com',
+    codeLink: 'https://github.com/ptrkbrn/todo-list',
+  },
+];
